a71b9ba934ff116c1322f4d4fb62a20e58baa81e
minarazi <razimina76@gmail.com>
2019-09-08 23:15:26 +0430

    remove unuse import

--- src/main/java/com/example/department/domain/Company.java
+++ src/main/java/com/example/department/domain/Company.java
@@ -1,16 +1,11 @@
 package com.example.department.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
